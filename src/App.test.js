3e57c98d36d8677da60b1887d798200798ba9a23
Kathryn Herod <kherod@idauto.net>
2022-10-25 20:10:16 -0500

    Fix app test for cicd

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const branding = screen.getByText('Kathryn Herod');
+  expect(branding).toBeInTheDocument();
 });
